b15357b9f29696955f394aa49e75a73bcd85b277
munachiani <mailstomuna@gmail.com>
2016-11-25 02:11:50 +0100

    signle disco view done

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -658,5 +658,14 @@ class AdminController extends Controller
 
     }
 
+    public function premesis($id){
+        $disco = DistributionCompany::find($id);
+        $notes = CustomerNote::where('disco_id','=',$disco->disco_name)->get();
+//                dd($notes);
+        return view('admin.singleDistribution')
+            ->with(compact('notes','disco'));
+
+    }
+
 
 }
